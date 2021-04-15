e489e863d51f15b1b4012637d3d12a53fd125cbe
michalg1410 <michalg1410@gmail.com>
2021-04-15 15:36:17 +0200

    REPOZYTORIUM

--- main.py
+++ main.py
@@ -11,25 +11,22 @@ x_p=1
 n=4
 
 def wezly_tab(x_i, n):
-    temp = (x_k - x_p) / (n - 1)
-    wezly = np.array([wezly, elementy])
+  
 
 
 # punkt3
-
-
-def generujTabliceGeometrii(x_p, x_k, n):
-    temp = (x_k - x_p) / (n - 1)
-    wezly = np.array([x_p, x_k])
+def tab_geo(x_p, x_k, n):
+    temp=(x_k-x_p) / (n - 1)
+    wezly=np.array([x_p, x_k])
 
     for i in range(1, n-1, 1):
-        wezly = np.block([wezly, i * temp + x_p])
+        wezly=np.block([wezly, i * temp + x_p])
 
-    elementy = np.array([])
-    n = n-1
+    elementy=np.array([])
+    n=n-1
 
     return wezly
-print(generujTabliceGeometrii(1,2,5))
+print(tab_geo(1,2,5))
 
 
 
